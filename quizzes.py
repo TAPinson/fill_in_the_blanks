046ec286400bccf80ba21375a0e610966f30edb7
Travis <travispinson@gmail.com>
2017-04-09 21:48:15 -0500

    progress for reading txt files. Current status: broken

---
+++ quizzes.py
@@ -0,0 +1,30 @@
+
+# 'easy' level
+easy_quiz = ['Build strong','___1___','solving skills! Start with finding the','___2___','. Then find the','___3___',
+            '. The only thing left to do now is to','___4___','the problem.']
+easy_answers = ['problem','inputs','outputs','solve']
+
+# 'medium' level
+medium_quiz = ['Defining a','___1___','is important to coding in','___2___','. Learning that your','___3___',
+               'arent global until you call the function with its','___4___','is equally important!']
+medium_answers = ['function','python','variables','parameters']
+
+# 'hard' level
+hard_quiz = ['To change a','___1___','variable, it needs to be processed by a',' ___2___',
+             '. Otherwise, it will remain ','___3___','to the original declaration. Process the variable change by',
+             '___4___ ','it as a parameter or the function.']
+hard_answers = ['global','function','equal','calling']
+
+blank_one = quiz[2]
+blank_two = quiz[9]
+blank_three = quiz[13]
+blank_four = quiz[24]
+answer_one = answers[0]
+answer_two = answers[1]
+answer_three = answers[2]
+answer_four = answers[3]
+first_spot = 2
+second_spot = 9
+third_spot = 13
+fourth_spot = 24
+response = raw_input('Fill in the blank: ')
