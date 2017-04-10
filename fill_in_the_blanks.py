7dcf487b23bfae958397405aae810c998a434c8c
Travis <travispinson@gmail.com>
2017-04-10 09:16:24 -0500

    refined to work with easy/medium/hard levels. Program will not handle user created template yet

--- fill_in_the_blanks.py
+++ fill_in_the_blanks.py
@@ -4,12 +4,12 @@ import os
 # responses against these lists
 quiz = []
 answers = []
-
 blank_one = "___1___"
 blank_two = "___2___"
 blank_three = "___3___"
 blank_four = "___4___"
 blank_five = "___5___"
+blanks = [blank_one, blank_two, blank_three, blank_four, blank_five]
 
 # Inputs: User is prompted to play again. # Outputs: If user elects to play again, global variables quiz and
 # answers are emptied and user is returned to the opening prompt
@@ -45,7 +45,7 @@ def game_engine(quiz,answers):
         print ' '.join(quiz)
         return game_engine(quiz, answers)
     elif response == answers[3]:
-        quiz = [blank.replace(blank_three, response) for blank in quiz]
+        quiz = [blank.replace(blank_four, response) for blank in quiz]
         print ' '.join(quiz)
         return next_level()
     else:
@@ -56,17 +56,34 @@ def game_engine(quiz,answers):
 # Inputs: user is prompted for response for difficulty.
 # Outputs: based on difficulty chosen, the global quiz list and answers list will be populated with the correct contents.
 def opening_prompt(quiz,answers):
-    response = raw_input('Would you like to play?(yes/no): ')
-    if response == 'yes':
-            with open ("easy-quiz.txt", "r") as quiz:
+    response = raw_input('Which level would you like to play?(easy/medium/hard/custom): ')
+    if response == 'easy':
+        with open ("easy-quiz.txt", "r") as quiz:
+            quiz = quiz.read().replace('\n', '')
+            quiz = quiz.split()
+            print ' '.join(quiz)
+        with open ("easy-answers.txt","r") as answers:
+            answers = answers.read().replace ('\n', '')
+            answers = answers.split()
+            return game_engine(quiz,answers)
+    elif response == 'medium':
+            with open ("medium-quiz.txt", "r") as quiz:
                 quiz = quiz.read().replace('\n', '')
                 quiz = quiz.split()
                 print ' '.join(quiz)
-            with open ("easy-answers.txt","r") as answers:
+            with open ("medium-answers.txt","r") as answers:
                 answers = answers.read().replace ('\n', '')
                 answers = answers.split()
-
                 return game_engine(quiz,answers)
+    elif response == 'hard':
+            with open("hard-quiz.txt", "r") as quiz:
+                quiz = quiz.read().replace('\n', '')
+                quiz = quiz.split()
+                print ' '.join(quiz)
+            with open("hard-answers.txt", "r") as answers:
+                answers = answers.read().replace('\n', '')
+                answers = answers.split()
+                return game_engine(quiz, answers)
     else:
         return opening_prompt(quiz,answers)
         #return opening_prompt(quiz,answers)
